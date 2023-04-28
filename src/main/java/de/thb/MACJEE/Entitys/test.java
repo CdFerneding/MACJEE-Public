3ce34f4fd018d048c49de6a7b69f84cac2a3e9c7
fconradmob <fernedin@th-brandenburg.de>
2023-04-28 17:33:25 +0200

    new test.java file in Entity

---
+++ src/main/java/de/thb/MACJEE/Entitys/test.java
@@ -0,0 +1,12 @@
+package de.thb.MACJEE.Entitys;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class test {
+    private String test;
+}
