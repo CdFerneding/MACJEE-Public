f02eec5079f583163a8f5f44fe3adb28025aa0db
fconradmob <fernedin@th-brandenburg.de>
2023-04-28 17:21:29 +0200

    Initial commit

---
+++ src/test/java/de/thb/MACJEE/MacjeeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thb.MACJEE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacjeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
