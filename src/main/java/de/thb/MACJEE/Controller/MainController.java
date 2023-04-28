99183166f9fdd3c95e9d0ca69f8270dafbd81905
TxRuX <maharneu@gmail.com>
2023-04-28 17:42:59 +0200

    printet Hallo

--- src/main/java/de/thb/MACJEE/Controller/MainController.java
+++ src/main/java/de/thb/MACJEE/Controller/MainController.java
@@ -1,4 +1,25 @@
 package de.thb.MACJEE.Controller;
 
-public class MainController {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.Serial;
+import java.util.List;
+@WebServlet("/macjee")
+public class MainController extends HttpServlet {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    public MainController(){
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append("Hallo Welt\r\n");
+    }
+
 }
+
