86044b9ce272d5a99408ce4fb51d987b1ad615a9
TxRuX <maharneu@gmail.com>
2023-05-01 22:13:47 +0200

    Verbesserung UI und Verlinkung zu An bzw. Ag Klasse Role zur Zuweisung der Rolle via Thymian

--- src/main/java/de/thb/MACJEE/Controller/MainController.java
+++ src/main/java/de/thb/MACJEE/Controller/MainController.java
@@ -1,27 +1,27 @@
 package de.thb.MACJEE.Controller;
 
+import de.thb.MACJEE.Entitys.Role;
 import jakarta.servlet.http.HttpServlet;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController extends HttpServlet {
-    /*
-    @Serial
-    private static final long serialVersionUID = 1L;*/
+
 
     public MainController(){
         super();
     }
 
-    /*
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().append("Hallo Welt\r\n");
-    }*/
 
+    /**
+     * Startseite mit mgl. zur Anmeldung und Registrierung
+     * @param model
+     */
     @GetMapping("/index")
     public void showIndex(Model model){
         /*model.addAttribute("notebooks", deskService.getAllNotebooks());
@@ -29,6 +29,11 @@ public class MainController extends HttpServlet {
         //return "index";
     }
 
+    /**
+     * Registrierungsformular Arbeitnehmer
+     * @param model
+     * @return
+     */
     @GetMapping("/registration_an")
     public String showRegistrationAn(Model model){
         /*model.addAttribute("notebooks", deskService.getAllNotebooks());
@@ -36,6 +41,11 @@ public class MainController extends HttpServlet {
         return "registration_an";
     }
 
+    /**
+     * Registrierungsformular Arbeitgeber
+     * @param model
+     * @return
+     */
     @GetMapping("/registration_ag")
     public String showRegistrationAg(Model model){
         /*model.addAttribute("notebooks", deskService.getAllNotebooks());
@@ -43,18 +53,35 @@ public class MainController extends HttpServlet {
         return "registration_ag";
     }
 
+    /**
+     * Registrierungsformular zur Determination der USER_ROLE --- Get request
+     * @param model
+     * @return
+     */
     @GetMapping("/registration_zero")
     public String showRegistrationZero(Model model){
+        model.addAttribute("role", new Role());
         return "registration_zero";
     }
 
+    /**
+     * Registrierungsformular zur Determination der USER_ROLE --- Post request
+     * @param role
+     * @return
+     */
     @PostMapping("/registration_zero")
-    public String getRegistrationZero(@PathVariable("role") int role, Model model){
-        if(role == 1) return "redirect:/registration_ag";
-        else if(role == 2) return "registration_an";
-        return "registration_zero";
+    public String getRegistrationZero(@ModelAttribute Role role){
+        if(role.role == 2) return "redirect:/registration_ag";
+        //System.out.print
+        else if(role.role == 1) return "registration_an";
+        return "redirect:/registration_zero";
     }
 
+    /**
+     * Anmeldungsformular mit input Benutzername und PW
+     * @param model
+     * @return
+     */
     @GetMapping("/login")
     public String showLogin(Model model){
         /*model.addAttribute("notebooks", deskService.getAllNotebooks());
