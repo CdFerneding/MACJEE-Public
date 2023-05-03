8427cc011c92e9baa9018e31a3c785902406be25
fconradmob <fernedin@th-brandenburg.de>
2023-05-03 20:13:17 +0200

    ersten Änderungen an Entitäten

--- src/main/java/de/thb/MACJEE/Entitys/CustomerVO.java
+++ src/main/java/de/thb/MACJEE/Entitys/CustomerVO.java
@@ -1,4 +1,18 @@
 package de.thb.MACJEE.Entitys;
 
+import de.thb.MACJEE.Enumerations.Characteristics;
+import de.thb.MACJEE.Enumerations.UserRole;
+
+import java.util.List;
+
 public class CustomerVO {
+    private long id;
+    private String mail;
+    private List<Characteristics> cahracteristics;
+    private UserRole userRole;
+
+    public void applyForJob(JobVO job){
+
+    }
+
 }
--- src/main/java/de/thb/MACJEE/Entitys/JobVO.java
+++ src/main/java/de/thb/MACJEE/Entitys/JobVO.java
@@ -1,4 +1,12 @@
 package de.thb.MACJEE.Entitys;
 
+import java.util.List;
+
 public class JobVO {
+    private long id;
+    private String title;
+    private List<Skill> requiredSkills;
+    private long salary;
+    private CompanyVO byCompany;
+    private List<CustomerVO> applicants;
 }
