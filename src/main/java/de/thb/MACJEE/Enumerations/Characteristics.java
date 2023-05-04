766fd6e5905d801dac6a510cef4f3c8ba951a074
15353453553 <albertzacher@posteo.de>
2023-05-04 14:19:08 +0200

    added subjects in Characteristics.java

--- src/main/java/de/thb/MACJEE/Enumerations/Characteristics.java
+++ src/main/java/de/thb/MACJEE/Enumerations/Characteristics.java
@@ -1,5 +1,7 @@
 package de.thb.MACJEE.Enumerations;
 
 public enum Characteristics {
-    MATH, BIOLOGY, PHYSICS
+    MATH, BIOLOGY, PHYSICS, GERMAN, CHEMISTRY, ENGLISH, PE, CS
+    // PE = PHYSICAL EDUCATION
+    // CS = COMPUTER SCIENCE
 }
