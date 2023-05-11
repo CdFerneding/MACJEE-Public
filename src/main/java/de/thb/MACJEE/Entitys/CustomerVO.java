a06988447d933444f49edd8063508c45c6a2bc8b
15353453553 <albertzacher@posteo.de>
2023-05-11 11:45:33 +0200

    added subjects in Characteristics.java

--- src/main/java/de/thb/MACJEE/Entitys/CustomerVO.java
+++ src/main/java/de/thb/MACJEE/Entitys/CustomerVO.java
@@ -3,13 +3,8 @@ package de.thb.MACJEE.Entitys;
 import de.thb.MACJEE.Entitys.Enumerations.Characteristics;
 import de.thb.MACJEE.Entitys.Enumerations.UserRole;
 import jakarta.persistence.*;
-
 import java.util.List;
-
-
-
 import lombok.*;
-
 import java.util.List;
 
 @Data
