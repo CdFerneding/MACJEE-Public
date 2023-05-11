597a45427b8ae214b49727efad27e4e598804fad
15353453553 <albertzacher@posteo.de>
2023-05-11 11:17:13 +0200

    added subjects in Characteristics.java

--- src/main/java/de/thb/MACJEE/Entitys/CustomerVO.java
+++ src/main/java/de/thb/MACJEE/Entitys/CustomerVO.java
@@ -2,13 +2,21 @@ package de.thb.MACJEE.Entitys;
 
 import de.thb.MACJEE.Entitys.Enumerations.Characteristics;
 import de.thb.MACJEE.Entitys.Enumerations.UserRole;
+import jakarta.persistence.*;
 
 import java.util.List;
 
+@Entity
+@Table(name="Customer")
 public class CustomerVO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
+    @Column(name = "mail")
     private String mail;
     private List<Characteristics> characteristics;
+    @Column(name = "userrole")
     private UserRole userRole;
 
     public void applyForJob(JobVO job){
