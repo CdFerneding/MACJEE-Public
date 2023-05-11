309c204bdda7bcc973e761f5dbea8e10d8ff6bb0
fconradmob <fernedin@th-brandenburg.de>
2023-05-11 16:46:44 +0200

    added some annotations

--- src/main/java/de/thb/MACJEE/Entitys/JobVO.java
+++ src/main/java/de/thb/MACJEE/Entitys/JobVO.java
@@ -1,10 +1,30 @@
 package de.thb.MACJEE.Entitys;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.annotation.Primary;
+
 import java.util.List;
 
+@Getter
+@Setter
+@Data
+@Table(name = "Job")
+@Entity
 public class JobVO {
+    @Id
+    @Column(name = "Id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column
     private String title;
+
+    @Column
     private List<Skill> requiredSkills;
     private long salary;
     private CompanyVO byCompany;
