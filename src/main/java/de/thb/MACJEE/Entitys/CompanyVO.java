ad74bb72978617b622fe9426977927dece463d7f
fconradmob <fernedin@th-brandenburg.de>
2023-05-11 16:46:44 +0200

    added some annotations

--- src/main/java/de/thb/MACJEE/Entitys/CompanyVO.java
+++ src/main/java/de/thb/MACJEE/Entitys/CompanyVO.java
@@ -1,6 +1,10 @@
 package de.thb.MACJEE.Entitys;
 
 import de.thb.MACJEE.Entitys.Enumerations.UserRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
 @Getter
@@ -8,10 +12,17 @@ import lombok.*;
 @NoArgsConstructor
 @Data
 public class CompanyVO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
     private long id;
+
+    private String companyName;
     private String firstName;
     private String lastName;
     private String userName;
+
+    @Column(name = "EMailAddress")
     private String mail;
     private String password;
     private String description;
@@ -19,6 +30,8 @@ public class CompanyVO {
     private String address2;
     private String country;
     private String state;
+
+    @Column(name = "postalCode")
     private int zip;
     //private String phoneNumber;
     // Fehler beim Template - erstmal weglassen
