94cc368fecd356a588ad5dd16f4145b79c5278f1
fconradmob <fernedin@th-brandenburg.de>
2023-05-16 15:01:15 +0200

    getting rid of Warnings upon loading MacjeeApplication (Springboot run).

--- src/main/java/de/thb/MACJEE/Entitys/CustomerSkillId.java
+++ src/main/java/de/thb/MACJEE/Entitys/CustomerSkillId.java
@@ -2,10 +2,12 @@ package de.thb.MACJEE.Entitys;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.Data;
 
 import java.io.Serializable;
 
 @Embeddable
+@Data
 public class CustomerSkillId implements Serializable {
     @Column(name = "customer_id")
     private Long customerId;
