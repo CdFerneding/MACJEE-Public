7376017651280e4933581782c163413855805ad4
fconradmob <fernedin@th-brandenburg.de>
2023-07-01 13:50:41 +0200

    matching jobs. siehe Dokumentation. Es fehlt noch: Die Sortierung der Jobs nach ihren ausgerechneten "differences" und die Einbeziehung von "isHardSkill".

--- src/main/java/de/thb/MACJEE/Controller/CustomerController.java
+++ src/main/java/de/thb/MACJEE/Controller/CustomerController.java
@@ -6,7 +6,6 @@ import de.thb.MACJEE.Entitys.Enumerations.Characteristics;
 import de.thb.MACJEE.Entitys.Job;
 import de.thb.MACJEE.Service.CustomerService;
 import de.thb.MACJEE.Service.JobFinder;
-import de.thb.MACJEE.Service.JobService;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -31,8 +30,6 @@ public class CustomerController {
     private final CustomerService customerService;
     @Autowired
     private final JobFinder jobFinder;
-    @Autowired
-    private JobService jobService;
 
     @GetMapping("/profile")
     public String showCustomerProfile (Model model) {
@@ -76,12 +73,13 @@ public class CustomerController {
             Customer customer = customerService.getCustomerByUserName(username)
                     .orElseThrow(() -> new UsernameNotFoundException("username not found."));
 
-            List<Job> jobs = jobService.getPerfectJob(customer);
+            List<Job> jobs = jobFinder.getPerfectJobs(customer);
             if (jobs != null) {
                 model.addAttribute("jobs", jobs);
-                redirectAttributes.addFlashAttribute("success", "Es wurde ein perfekter Job für dich gefunden.");
+                model.addAttribute("success", "Es wurde ein perfekter Job für dich gefunden.");
             } else {
-                redirectAttributes.addFlashAttribute("error", "Es wurde KEIN perfekter Job für dich gefunden.");
+                model.addAttribute("error", "Es wurde KEIN perfekter Job für dich gefunden. ");
+                return "/user/dashboard";
             }
         } catch (UsernameNotFoundException e) {
             redirectAttributes.addFlashAttribute("error", "there is a problem with the currently logged in user.");
--- src/main/java/de/thb/MACJEE/Service/JobFinder.java
+++ src/main/java/de/thb/MACJEE/Service/JobFinder.java
@@ -1,23 +1,25 @@
 package de.thb.MACJEE.Service;
 
+import de.thb.MACJEE.Entitys.Enumerations.Characteristics;
 import de.thb.MACJEE.Repository.*;
 import de.thb.MACJEE.Entitys.*;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Service
 @Data
 public class JobFinder {
     @Autowired
-    private final CustomerRepository customerRepository;
+    private CustomerRepository customerRepository;
     @Autowired
-    private final JobRepository jobRepository;
+    private JobRepository jobRepository;
+    @Autowired
+    private JobService jobService;
 
-    public List<Job> findPerfectJobs(Long customerId) throws Exception{
+    public List<Job> findPerfectJobs(Long customerId) throws Exception {
 
         // 1. Lese den Customer aus der Datenbank
         Customer customer = customerRepository.findById(customerId).orElseThrow(() -> new IllegalArgumentException("Customer not found: " + customerId));
@@ -29,6 +31,103 @@ public class JobFinder {
         List<Job> matchingJobs = jobRepository.findBySkillsIn(customerSkills);
 
         return matchingJobs;
+    }
+
+    public List<Job> getPerfectJobs(Customer customer) {
+        List<Job> openJobs = jobService.getOpenJobs();
+        Map<Job, Integer> jobDiffMap = new HashMap<>();
+
+        if (!openJobs.isEmpty()) {
+            List<Skill> skills = customer.getSkills();
+
+            for (Job job : openJobs) {
+                List<Skill> requiredSkills = job.getRequiredSkills();
+
+                // calculate difference between skill lists
+                Integer difference = calculateDifference(skills, requiredSkills);
+                if (difference == null) {
+                    // if there is a problem with calculating the difference the job is marked as under-qualified
+                    jobDiffMap.put(job, Characteristics.getNumberOfCharacteristics()*15);
+                }
+                jobDiffMap.put(job, difference);
+            }
+        } else return null;
+
+        // sort the map entries based on the absolute difference to 0 ...
+
+
+        // add the first 5 jobs to
+        List<Job> matchingJobs = new ArrayList<>();
+        int count = 0;
+        if(jobDiffMap.size() < 5) {
+            count = jobDiffMap.size();
+        }
+
+        for (Map.Entry<Job, Integer> entry : jobDiffMap.entrySet()) {
+            if (count >= 5) {
+                break;  // Exit the loop once 5 jobs have been added
+            }
+
+            Job job = entry.getKey();
+            matchingJobs.add(job);
+
+            count++;
+        }
+
+        return matchingJobs;
+    }
+
+
+    /**
+     * @param skills
+     * @param requiredSkills
+     * @return integer value that indicates:
+     * below zero: over-qualified
+     * above zero: under-qualified
+     * the closer the value is to zero the more the customer matches the job
+     */
+    private Integer calculateDifference(List<Skill> skills, List<Skill> requiredSkills) {
+        if (requiredSkills == null || skills == null) {
+            return null;
+        }
+        /**
+         * when no skills are required all skills of customer are essentially over-qualification
+         * note that if a job has no required skills the customer will be greatly overqualified!!
+         * (meaning are high value below zero is returned)
+         */
+        if (skills.size() != 0) {
+            if (requiredSkills.size() == 0) {
+                int sum = 0;
+                for (Skill skill : skills) {
+                    sum += skill.getLevel();
+                }
+                return -1 * sum;
+            }
+        }
+        if (skills.size() == 0) {
+            return null;
+        }
+        // either the job or the customer skills are not fully initialized
+        int numberOfSkills = Characteristics.getNumberOfCharacteristics();
+        if(skills.size() != numberOfSkills || requiredSkills.size() != numberOfSkills) {
+            return null;
+        }
+        int[] skillsDiff = new int[skills.size()];
+        // calculate the difference between individual skills
+        for (int i = 0; i < skills.size(); i++) {
+            int differenceOfSkill = (int) (requiredSkills.get(i).getLevel() - skills.get(i).getLevel());
+            skillsDiff[i] = differenceOfSkill;
+        }
+        // add the individual differences to get the overall difference
+        int jobDiff = 0;
+        for (int i = 0; i < skills.size(); i++) {
+            /* no differentiation between positive and negative difference!
+             * Because if you are over-qualified for one skill the skillDiff will be below zero.
+             * It will cancel out the skills that are above zero (the skills where the customer is under-qualified).
+             */
+            jobDiff += skillsDiff[i];
+        }
+        return jobDiff;
     }
 }
 
